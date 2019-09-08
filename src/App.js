ade237ea829d745633583cc56873ddc4af48e4ad
samwx <samuelmartins.sw@gmail.com>
2019-09-08 13:53:55 -0300

    Add links to cart and home page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Suspense, lazy } from "react";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import "./App.css";
 import { ProductsList } from "./views/ProductsList";
 import { Store } from "./store";
 import Cart from "./views/Cart";
+import { CartCounter } from "./components/CartCounter";
 const Product = lazy(() => import("./views/Product"));
 
 function App() {
@@ -14,6 +15,10 @@ function App() {
         <Suspense fallback={<h1>Loading...</h1>}>
           <h1>Amazing store</h1>
           <BrowserRouter>
+            <div>
+              <Link to="/">Home</Link> |
+              <CartCounter />
+            </div>
             <Route exact path="/" component={ProductsList} />
             <Route path="/product/:id" component={Product} />
             <Route path="/cart" component={Cart} />
